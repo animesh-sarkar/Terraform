0cc49d8741a2e447023c3a9a68dcbc17b4366bea
Animesh Sarkar <124586589+animesh-sarkar@users.noreply.github.com>
2025-06-07 11:54:48 +0530

    Create Vnet.tf

---
+++ Vnet.tf
@@ -0,0 +1,26 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
