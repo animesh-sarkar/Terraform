5043bea19ae3011241ed5fbfdbc9f0db746c262e
Animesh Sarkar <124586589+animesh-sarkar@users.noreply.github.com>
2025-06-07 12:02:45 +0530

    Create Vnet_with_Subnet.tf

---
+++ Vnet_with_Subnet.tf
@@ -0,0 +1,55 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create subnet 1
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create subnet 2
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Output the VNet and subnet IDs
+output "vnet_id" {
+  value = azurerm_virtual_network.example.id
+}
+
+output "subnet1_id" {
+  value = azurerm_subnet.subnet1.id
+}
+
+output "subnet2_id" {
+  value = azurerm_subnet.subnet2.id
+}
